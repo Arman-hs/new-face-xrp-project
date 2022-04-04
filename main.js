4f074915a6623ff0c3fb260ce3d5feed04d0cc8e
Arash Hajisafi <hajisafiarash@gmail.com>
2022-04-04 03:47:29 -0700

    Clipboard buttons and function implemented

--- main.js
+++ main.js
@@ -1,49 +1,8 @@
-  
-const copyButton1 = document.getElementById('btncopy');
-
-copyButton1.addEventListener('click', (event) =>{
-const Numb = document.getElementById('numb').textContent;
-navigator.clipboard.writeText(Numb);
-})
-
-const copyButton2 = document.getElementById('btncopy_1');
-
-copyButton2.addEventListener('click', (event) =>{
-const Tag = document.getElementById('tag').textContent;
-navigator.clipboard.writeText(Tag);
-})
-// var image = document.getElementById('btncopy');
-
-//     image.addEventListener('click', function(){
-//         changeImage();
-//         setTimeout(() => {
-//             changeImage_1();
-//         }, 2500);
-       
-//     });
-
-//     function changeImage(){
-//             image.src = 'copy-icon-4786.png';
-            
-//     }
-//     function changeImage_1(){
-//             image.src = 'transparent-clipboard-icon-copy-icon-duplicate-icon-5db75fcfbf2c48.4955590615722987037831.png'
-
-//     }
-    
-var icon = document.getElementsByClassName("fa-regular fa-clipboard");
-
-    icon.addEventListener('click', function(){
-        Change();
-        setTimeout(() => {
-            change_2();
-        }, 2500);
-
-    });
-
-    function Change(){
-        icon.className = "fa-solid fa-clipboard-check";
-    }
-    function change_2(){
-        icon.className = "bi bi-clipboard";
-    }
+  function copy_and_change(icon_elem, elemid_to_copy) {
+      // copy to clipboard
+      let elem_content = document.getElementById(elemid_to_copy).textContent;
+      navigator.clipboard.writeText(elem_content);
+      // change icon
+      icon_elem.className = "fa-solid fa-clipboard-check";
+      setTimeout(() => icon_elem.className = "fa-regular fa-clipboard", 2000)
+  }
