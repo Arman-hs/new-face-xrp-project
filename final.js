04c10f516605e9324a2284abc8cc58f78db3efef
Arman-hs <armanhajisafi@gmail.com>
2022-04-10 12:27:53 +1000

    made responsive

---
+++ final.js
@@ -0,0 +1,8 @@
+function copy_and_change(icon_elem, elemid_to_copy) {
+    // copy to clipboard
+    let elem_content = document.getElementById(elemid_to_copy).textContent;
+    navigator.clipboard.writeText(elem_content);
+    // change icon
+    icon_elem.className = "fa-solid fa-clipboard-check";
+    setTimeout(() => icon_elem.className = "fa-regular fa-clipboard", 2000)
+}
