721a11faf1c082a843e91d45cd3c4c0f92459762
Arman-hs <armanhajisafi@gmail.com>
2022-04-29 12:32:00 +1000

    index/result page fixed Ver1

--- final.js
+++ final.js
@@ -1,12 +1,19 @@
-function copy_and_change(icon_elem, elemid_to_copy) {
+function copyToClipboard(text) {
+    elem = document.getElementById('temp_text')
+    elem.style.visibility = 'visible'
+    elem.value = text
+    elem.select();
+    elem.setSelectionRange(0, 99999); /* For mobile devices */
+    document.execCommand('copy');
+    elem.value = ''
+    elem.style.visibility = 'hidden'
+    // navigator.clipboard.writeText(text);
+ } function copy_and_change(icon_elem, elemid_to_copy) {
     // copy to clipboard
     let elem_content = document.getElementById(elemid_to_copy).textContent;
-    navigator.clipboard.writeText(elem_content);
+    // navigator.clipboard.writeText(elem_content);
+    copyToClipboard(elem_content)
     // change icon
-    icon_elem.className = "fa-solid fa-clipboard-check";
-    setTimeout(() => icon_elem.className = "fa-regular fa-clipboard", 2000)
-}
-
-function hi(){
-    alert('hi');
+    icon_elem.className = "fa-solid fa-clipboard-check fa-lg";
+    setTimeout(() => icon_elem.className = "fa-regular fa-clipboard fa-lg", 2000)
 }
